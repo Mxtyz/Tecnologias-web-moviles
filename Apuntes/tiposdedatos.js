ada2e8bdb65ca068b9e49f15a5f6a16648b8fdf3
Mxtyz5 <Mxtyz@users.noreply.github.com>
2023-09-13 17:39:56 -0300

    Nuevos Archivos

---
+++ Apuntes/tiposdedatos.js
@@ -0,0 +1,28 @@
+//NUMEROS
+let edad = 29;
+let sueldo = 1200000;
+const PI = 3.14;
+
+//Con Notacion cientifica 
+const n_grande = 1e6; // 1 millon 
+const n_pequeño = 1e-6; // 0.000001
+
+console.log(n_grande);
+console.log(n_pequeño);
+
+//BigInt
+const BigInt = 123456789123456789123456789;
+console.log(BigInt);
+
+//Infinito
+console.log(edad / 0);
+
+//STRINGS
+console.log('################ STRINGS ################')
+let nombre = "Victor";
+let ciudad = "Osorno";
+let capital = "Puerto Montt";
+let string1 = "Hola, como estas?";
+let string2 = "!Buenas tardes!";
+
+
---
+++ Apuntes/variables.js
@@ -0,0 +1,13 @@
+//Estas variables pueden cambiar 
+var estatura = 1.71
+let peso = 68
+
+// Variable fija 
+const apellido = "Perez"
+
+var edad 
+var ciudad
+
+//Iniciar variables
+edad = 19
+ciudad = "Osorno"
